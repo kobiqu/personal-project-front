9f47ad15db616ad273b8eff80fe88773db0621ae
kobiqu <kobiquinn50>
2023-12-18 13:22:33 +0200

    first atage

---
+++ src/feauters/main_page/header/style/header_style.ts
@@ -0,0 +1,42 @@
+import { CSSProperties } from "react";
+
+export const header_style_1: CSSProperties | undefined = {
+  position: "sticky",
+  backgroundColor: "white",
+  top: "0",
+  height: "26vh",
+  minWidth: "98.6vw",
+  maxWidth: "100vw",
+  marginTop: "-0.57%",
+  marginLeft: "-0.5%",
+  border: "0.2px solid black transparent",
+  boxShadow: "0 5px 2px -2px  rgb(75, 69, 69)",
+  zIndex: "8000",
+};
+export const header_style_2: CSSProperties | undefined = {
+  backgroundColor: "orangered",
+  position: "relative",
+  top: "0%",
+  width: "100%",
+  height: "19%",
+};
+export const header_style_3: CSSProperties | undefined = {
+  position: "absolute",
+  top: "12%",
+  left: "4%",
+};
+export const header_style_4: CSSProperties | undefined = {
+  fontFamily: "cursive",
+  fontStyle: "oblique",
+  marginBottom: "-12%",
+};
+export const header_style_5: CSSProperties | undefined = {};
+export const header_style_6: CSSProperties | undefined = {};
+
+export const header_img_1: CSSProperties | undefined = {
+  position: "absolute",
+  top: "19%",
+  right: "0%",
+  height: "81%",
+  width: "20%",
+};
